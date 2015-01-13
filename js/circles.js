b549a7798ee41d6afa91307e0627962faf897a90
gabedb <gabedb@me.com>
2015-01-12 19:12:56 -0600

    current version of the site

---
+++ js/circles.js
@@ -0,0 +1,19 @@
+$( document ).ready(function() {
+    var div = 360 / 6;
+    var radius = 150;
+    var parentdiv = document.getElementById('circles');
+    var offsetToParentCenter = parseInt(parentdiv.offsetWidth / 2);  //assumes parent is square
+    var offsetToChildCenter = 50;
+    var totalOffset = offsetToParentCenter - offsetToChildCenter;
+    for (var i = 1; i <= 6; ++i)
+    {
+    var childdiv = document.createElement('div');
+    childdiv.className = 'div2';
+    childdiv.style.position = 'absolute';
+    var y = Math.sin((div * i) * (Math.PI / 180)) * radius;
+    var x = Math.cos((div * i) * (Math.PI / 180)) * radius;
+    childdiv.style.top = (y + totalOffset).toString() + "px";
+    childdiv.style.left = (x + totalOffset).toString() + "px";
+    parentdiv.appendChild(childdiv);
+    }
+});
