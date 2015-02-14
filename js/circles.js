76ef1db23ff246dbeb2b96657b227cac7fd068f0
Lawrence Humphrey <lawrencehumphrey@Lawrences-MacBook-Pro-2.local>
2015-02-14 17:24:42 -0600

    obligatory; navbar and testimonials

--- js/circles.js
+++ js/circles.js
@@ -1,26 +1,38 @@
-var offset = 57;
+/*global $ */
 
-$(".navbar ul li a[href^='#']").on('click', function(e) {
+var offset = 53;
+var toggled = false;
 
-   // prevent default anchor click behavior
-   e.preventDefault();
+$(".image, .gal_overlay").click(function () {
+//    alert('working');
+    "use strict";
+    $(".gal_overlay").toggleClass("active");
+});
 
-   // store hash
-   var hash = this.hash;
+$(".navbar ul li a[href^='#']").on('click', function (e) {
 
-   // animate
-   $('html, body').animate({
-       scrollTop: $(hash).offset().top - offset
-     }, 500, function(){
-
-       // when done, add hash to url
-       // (default click behaviour)
-       window.location.hash = hash;
-     });
+    // prevent default anchor click behavior
+    "use strict";
+    e.preventDefault();
+ 
+    // store hash
+    var hash = this.hash;
+ 
+    // animate
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top - offset
+    }, 500, function () {
+ 
+        // when done, add hash to url
+        // (default click behaviour)
+        window.location.hash = hash;
+    });
 
 });
 
-$( window ).resize(function(){
+
+$(window).resize(function strict() {
+    "use strict";
     var cw = $('.sq-pic').width();
     $('.sq-pic').css({
         'height': cw + 'px'
@@ -35,21 +47,21 @@ $( window ).resize(function(){
     });
 });
 
-$('.ignore').css({'visibility':'hidden', 'width':'0%'});
+$('.ignore').css({'visibility': 'hidden', 'width': '0%'});
 
-$( document ).ready( function(){
-    var cw = $('.sq-pic').width();
-    $('.sq-pic').css({
-        'height': cw + 'px'
-    });
-    
-    var h = $('.quad-2').height();
-    $('.quad-1').css({
-        'height': h + 'px'
-    });
-    $('.quad-1 img').css({
-        'height': h + 'px'
-    });
+$(document).ready(function () {
+//    var cw = $('.sq-pic').width();
+//    $('.sq-pic').css({
+//        'height': cw + 'px'
+//    });
+//    
+//    var h = $('.quad-2').height();
+//    $('.quad-1').css({
+//        'height': h + 'px'
+//    });
+//    $('.quad-1 img').css({
+//        'height': h + 'px'
+//    });
 //    var h = $('#gpic2').height();
 //    $('#gpic1').css({
 //        'height': h + 'px'
@@ -58,4 +70,28 @@ $( document ).ready( function(){
     $('#carousel-example-generic').fadeTo(2500, 1);
     $('.my-cap').fadeTo(3000, 1);
     
+});
+
+$(window).load(function () {
+    "use strict";
+    var cw = $('.sq-pic').width();
+    $('.sq-pic').css({
+        'height': cw + 'px'
+    });
+    
+    var h = $('.quad-2').height();
+    $('.quad-1').css({
+        'height': h + 'px'
+    });
+    $('.quad-1 img').css({
+        'height': h + 'px'
+    });
+    
+//    var h2 = $('#gpic2').height();
+//    $('#trouble').css({
+//        'height': h2 + 'px'
+//    });
+//    $('#trouble img').css({
+//        'height': h2 + 'px'
+//    });
 });
